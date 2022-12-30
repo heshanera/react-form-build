98c3589a3a988d50ea20f78559c631e9536096bc
Heshan Eranga <erangamx@gmail.com>
2022-12-30 12:43:17 +0530

    range input element

--- src/lib/components/ElementTypes/index.jsx
+++ src/lib/components/ElementTypes/index.jsx
@@ -3,11 +3,13 @@ import DIVIDER from './Divider';
 import MULTILINE_INPUT from './TextArea';
 import TEXT from './Text';
 import SELECT from './Select';
+import RANGE from './Range';
 
 const ElementTypes = {
   INPUT,
   MULTILINE_INPUT,
   SELECT,
+  RANGE,
   TEXT,
   DIVIDER,
 };
